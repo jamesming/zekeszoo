46d33440d80d6df1d6ea5353827c42da2f1abde0
James Ming <jamesming@gmail.com>
2011-12-03 07:03:31 -0800

    added

--- application/views/home/contactus_view.php
+++ application/views/home/contactus_view.php
@@ -51,7 +51,7 @@
 																																		}else{     ?>
 																																			
 																																			
-																	
+																	test
 
 																																			
 																																			<?php
