6c04a1a0dd705ca4c035db87d122fac342a085cf
unknown <James Ming@JamesMing-THINK.(none)>
2012-02-16 14:34:00 -0800

    added

--- application/views/home/account_menu.php
+++ application/views/home/account_menu.php
@@ -87,11 +87,7 @@ a.myaccount.menu-open span {
 }		
 
 </style>
-<script type="text/javascript" language="Javascript">
-	$(document).ready(function() { 
-		$.cookie("joined", '1');
-	});
-</script>
+
 <div id='myaccount_container'  >
 	
  <div id="topnav" class="topnav">
